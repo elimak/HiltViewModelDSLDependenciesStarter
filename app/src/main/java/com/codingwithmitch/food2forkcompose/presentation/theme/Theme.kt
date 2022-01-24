9b4481cbdeed8f6542ee5fca551177d663cfce54
lucie <lu@>
2022-01-24 08:25:49 +0100

    default snackbar

--- app/src/main/java/com/codingwithmitch/food2forkcompose/presentation/theme/Theme.kt
+++ app/src/main/java/com/codingwithmitch/food2forkcompose/presentation/theme/Theme.kt
@@ -1,16 +1,19 @@
 package com.codingwithmitch.food2forkcompose.presentation.theme
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.codingwithmitch.food2forkcompose.presentation.components.DefaultSnackbar
+import kotlinx.coroutines.launch
 import java.util.*
 
 private val LightThemeColors = lightColors(
@@ -50,6 +53,8 @@ fun AppTheme(
   scaffoldState: ScaffoldState,
   content: @Composable () -> Unit,
 ) {
+
+  val coroutineScope = rememberCoroutineScope() // NEW TEST
   MaterialTheme(
     colors = if (darkTheme) DarkThemeColors else LightThemeColors,
     typography = QuickSandTypography,
@@ -64,6 +69,26 @@ fun AppTheme(
 
         content()
       }
+
+      /////////////////////////////////////////////////// NEW
+      Button(
+        onClick = {
+          coroutineScope.launch { // using the `coroutineScope` to `launch` showing the snackbar
+            // taking the `snackbarHostState` from the attached `scaffoldState`
+            val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
+              message = "This is your message",
+              actionLabel = "Do something."
+            )
+            when (snackbarResult) {
+              SnackbarResult.Dismissed -> Log.d("SnackbarDemo", "Dismissed")
+              SnackbarResult.ActionPerformed -> Log.d("SnackbarDemo", "Snackbar's button clicked")
+            }
+          }
+        }
+      ) {
+        Text(text = "A button that shows a Snackbar")
+      }
+    //}/////////////////////////////////////////////   NEW
 
       DefaultSnackbar(
         snackbarHostState = scaffoldState.snackbarHostState,
