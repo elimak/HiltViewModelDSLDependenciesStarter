e1d7acaf5603c7f9a9285eda087b8edd8b9d5d23
lucien <lucien@web.de>
2022-02-16 12:20:48 +0100

    interactors module - problem with ktor

---
+++ app/src/main/java/de/apps/LittleNemo/di/InteractorsModule.kt
@@ -0,0 +1,38 @@
+package de.apps.LittleNemo.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import de.apps.LittleNemo.interactors.picture_collection.SearchPictureCollection
+import de.apps.LittleNemo.shared.datasource.network.RecipeService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object InteractorsModule {
+
+    @Singleton
+    @Provides
+    fun provideSearchRecipes(
+        recipeService: RecipeService,
+        //recipeCache: RecipeCache,
+    ): SearchPictureCollection {
+        return SearchPictureCollection(
+            recipeService = recipeService,
+            //recipeCache = recipeCache
+        )
+    }
+
+    /*
+    @InternalAPI
+    @Singleton
+    @Provides
+    fun provideGetRecipe(
+        recipeCache: RecipeCache,
+    ): GetRecipe {
+        return GetRecipe(
+            recipeCache = recipeCache
+        )
+    }*/
+}
