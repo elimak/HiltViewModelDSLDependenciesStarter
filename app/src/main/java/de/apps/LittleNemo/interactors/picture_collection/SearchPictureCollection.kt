a155de135f37ce68ce0bff7c5993bd171fb72aee
lucien <lucien@web.de>
2022-02-16 12:10:45 +0100

    Search Picture collection interactor

---
+++ app/src/main/java/de/apps/LittleNemo/interactors/picture_collection/SearchPictureCollection.kt
@@ -0,0 +1,135 @@
+package de.apps.LittleNemo.interactors.picture_collection
+
+import de.apps.LittleNemo.domain.data.DataState
+import de.apps.LittleNemo.domain.model.Recipe
+import de.apps.LittleNemo.shared.datasource.network.RecipeService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
+class SearchPictureCollection(
+    private val recipeService: RecipeService,
+    //private val recipeCache: RecipeCache,
+) {
+    fun execute(
+       // token: String,
+        page: Int,
+        query: String,
+    ): Flow<DataState<List<Recipe>>> = flow {
+        try {
+            //emit(DataState.loading())
+
+            // just to show pagination, api is fast
+//      delay(1000)
+
+    /*
+            // force error for testing
+            if (query == "error") {
+                throw Exception("Search FAILED!")
+            }
+*/
+            try{
+
+                val recipes = recipeService.search(
+                    page = page,
+                    query = query,
+                )
+                val test = recipes
+                /*
+                // Convert: NetworkRecipeEntity -> Recipe -> RecipeCacheEntity
+                val recipes = getRecipesFromNetwork(
+                  //  token = token,
+                    page = page,
+                    query = query,
+                )*/
+
+                // insert into cache
+                //recipeDao.insertRecipes(entityMapper.toEntityList(recipes))
+            }catch (e: Exception){
+                // There was a network issue
+                e.printStackTrace()
+            }
+
+            /*
+            // query the cache
+            val cacheResult = if (query.isBlank()) {
+                recipeDao.getAllRecipes(
+                    pageSize = RECIPE_PAGINATION_PAGE_SIZE,
+                    page = page
+                )
+            } else {
+                recipeDao.searchRecipes(
+                    query = query,
+                    pageSize = RECIPE_PAGINATION_PAGE_SIZE,
+                    page = page
+                )
+            }
+
+            // emit List<Recipe> from cache
+            val list = entityMapper.fromEntityList(cacheResult)
+
+            emit(DataState.success(list))
+
+             */
+        } catch (e: Exception) {
+            //emit(DataState.error<List<Recipe>>(e.message ?: "Unknown Error"))
+        }
+    }
+
+}
+
+
+
+
+/*
+    fun execute(
+        page: Int,
+        query: String,
+    ): CommonFlow<DataState<List<Recipe>>> = flow  {
+        try{
+            //emit(DataState.loading())
+
+            // just to show pagination, api is fast
+            delay(500)
+
+            // force error for testing
+            if (query == "error") {
+                throw Exception("Forcing an error... Search FAILED!")
+            }
+
+            val recipes = recipeService.search(
+                page = page,
+                query = query,
+            )
+            val test = recipes
+
+            // insert into cache
+            recipeCache.insert(recipes)
+
+            // query the cache
+            val cacheResult = if (query.isBlank()) {
+                recipeCache.getAll(page = page)
+            } else {
+                recipeCache.search(
+                    query = query,
+                    page = page,
+                )
+            }
+
+
+            // emit List<Recipe> from cache
+            emit(DataState.data<List<Recipe>>(message = null, data = cacheResult))
+
+
+        } catch (e: Exception) {
+
+            emit(DataState.error<List<Recipe>>(
+                message = GenericMessageInfo.Builder()
+                    .id("SearchRecipes.Error")
+                    .title("Error")
+                    .uiComponentType(UIComponentType.Dialog)
+                    .description(e.message?: "Unknown Error")
+            ))
+        }
+    }.asCommonFlow()
+*/
--- app/src/main/java/de/apps/LittleNemo/presentation/ui/PictureCollectionViewModel.kt
+++ app/src/main/java/de/apps/LittleNemo/presentation/ui/PictureCollectionViewModel.kt
@@ -1,15 +1,15 @@
 package de.apps.LittleNemo.presentation.ui
 
 
-import android.content.Intent
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import de.apps.LittleNemo.presentation.Picture
 import de.apps.LittleNemo.presentation.PicturesState
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import de.apps.LittleNemo.interactors.picture_collection.SearchPictureCollection
 import javax.inject.Inject
 
 
@@ -19,7 +19,7 @@ import javax.inject.Inject
 class PictureCollectionViewModel
 @Inject
 constructor(
-
+    private val searchPictureCollection: SearchPictureCollection
 
 ) : ViewModel() {
 
@@ -28,8 +28,33 @@ constructor(
 
     init {
         picturesState.value = PicturesState.pictures
+        loadRecipes()
     }
 
+
+    ///////
+    private fun loadRecipes(){
+        searchPictureCollection.execute(
+            page = 1,
+            query = ""
+        )
+            /*
+            .collectCommon(viewModelScope) { dataState ->
+            state.value = state.value.copy(isLoading = dataState.isLoading)
+
+            dataState.data?.let { recipes ->
+                appendRecipes(recipes)
+            }
+
+            dataState.message?.let { message ->
+                appendToMessageQueue(message)
+            }
+        }*/
+    }
+
+
+
+    ///////
 
     fun nextPicture(){
         currentPictureIndex.value++
